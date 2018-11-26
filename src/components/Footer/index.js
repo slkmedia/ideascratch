c466dc2bef10429df23c99cd92d4ce23f3a2372c
Kyle McDonald <hello@kylemcd.com>
2018-11-26 17:46:15 -0600

    Add coffee button

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-import { Footer } from './styled';
+import { 
+  Footer,
+  CoffeeButton
+} from './styled';
 
 export default () => {
   return (
     <Footer>
       Created by <a href="https://twitter.com/designbykyle">@designbykyle</a> &{' '}
       <a href="https://twitter.com/sunnysinghio">@sunnysinghio</a>
+      <div>
+      <CoffeeButton class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/kylemcd">
+        <img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee"/>
+        <div>Buy us a coffee</div>
+      </CoffeeButton>
+      </div>
     </Footer>
   );
 };
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -5,3 +5,35 @@ export const Footer = styled.div`
   text-align: center;
   margin: 32px 0;
 `;
+
+export const CoffeeButton = styled.a`
+  align-items: center;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  color: #333 !important;
+  cursor: pointer;
+  display: flex;
+  font-size: 16px;
+  margin: 30px auto 0 auto;
+  padding: 12px 16px;
+  text-align: center;
+  transition: all 0.2s ease-in-out;
+  width: 150px;
+
+  div {
+    margin-left: 5px;
+  }
+
+  &:hover,
+  &:focus {
+    border: 1px solid #0482ff;
+    color: #0482ff !important;
+    text-decoration: none !important;
+  }
+
+  &:focus {
+    transform: scale(0.97);
+    outline: none;
+  }
+`;
