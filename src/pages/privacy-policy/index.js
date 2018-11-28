72abc6666623f08b93ba1395966c3b8080db9de7
Kyle McDonald <hello@kylemcd.com>
2018-11-27 19:42:10 -0600

    add ga to privacy policy

--- src/pages/privacy-policy/index.js
+++ src/pages/privacy-policy/index.js
@@ -43,6 +43,13 @@ export default function PrivacyPolicyPage() {
         questions about how we handle user data and personal information, feel
         free to contact us.
       </p>
+      <p>
+        Google Analytics is a web analytics service offered by Google that tracks and reports website traffic. Google uses the data collected to track and monitor the use of our Service. This data is shared with other Google services. Google may use the collected data to contextualize and personalize the ads of its own advertising network.
+      </p>
+
+      <p>You can opt-out of having made your activity on the Service available to Google Analytics by installing the Google Analytics opt-out browser add-on. The add-on prevents the Google Analytics JavaScript (ga.js, analytics.js, and dc.js) from sharing information with Google Analytics about visits activity.</p>
+
+      <p>For more information on the privacy practices of Google, please visit the Google Privacy Terms web page: <a href="https://policies.google.com/privacy?hl=en">https://policies.google.com/privacy?hl=en</a></p>
       <p>This policy is effective as of 27 November 2018.</p>
     </Fragment>
   );
